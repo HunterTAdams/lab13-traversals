cc433d0c42470c902b1d5ada89eb917ff5e24d36
Keerthanaa Ellur <Keerthanaa.ellur@gmail.com>
2022-05-06 12:05:54 -0700

    stash

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -137,12 +137,13 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//then recursively print the right side of current node
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
+		System.out.println("InOrder test commit");
 		inOrderRecurse(root); 
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) 
 	{
-		System.out.println("InOrder test commit");
+		
 		
 	}
 	//Traverse the tree in an inorder fashion but using a stack
